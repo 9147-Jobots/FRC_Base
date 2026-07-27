3ceaba5b347cf20d34d9a18b99b9c7652617da6a
Timothy Shu <tims11105@gmail.com>
2026-07-27 21:33:27 +1000

    added usb logging, to be tested

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,9 +4,12 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import java.io.File;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
@@ -14,6 +17,13 @@ public class Robot extends TimedRobot {
   private final RobotContainer m_robotContainer;
 
   public Robot() {
+    File usb = new File("/U");
+    if (usb.exists() && usb.isDirectory()) {
+      DataLogManager.start("/U");
+    } else {
+      DriverStation.reportWarning("No USB drive found, logging disabled", false);
+    }
+
     m_robotContainer = new RobotContainer();
   }
 
